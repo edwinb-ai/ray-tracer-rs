81f4cb99128bb070e5911a43341dfdda83fd1d56
Edwin Bedolla <developeredwin@gmail.com>
2022-06-06 12:27:45 -0500

    :coffin:

--- lib/src/tuple/mod.rs
+++ lib/src/tuple/mod.rs
@@ -11,10 +11,6 @@ pub use vector::Vector;
 /// The `Tuple` trait is used to implement
 /// either a `Vector` (which has `w` value of 0.0)
 /// and a `Point` (which as a `w` value of 1.0).
-///
-/// `get_x`, `get_y`, `get_z` and `get_w` are
-/// all "getters" for the types that implement
-/// this trait.
 pub trait Tuple: PartialEq {
     fn origin() -> Self;
     fn new(x: f64, y: f64, z: f64) -> Self;
